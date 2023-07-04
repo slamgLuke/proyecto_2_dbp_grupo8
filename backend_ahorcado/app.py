45d3d67794b2835007c11dbdcc7e6c767d9d0508
DavidHerencia <david.herencia@utec.edu.pe>
2023-07-04 13:46:42 -0500

    jsonify login bien hecho

--- backend_ahorcado/app.py
+++ backend_ahorcado/app.py
@@ -226,11 +226,11 @@ def player_login():
         if player.password == input_player['password']:
             player.logged_in = True
             db.session.commit()
-            return jsonify('{"response": "' + str(player.id) + '"}')
+            return jsonify({"response": str(player.id)})
         else:
-            return jsonify('{"response": "FAIL"}')
+            return jsonify({"response": "FAIL"})
     else:
-        return jsonify('{"response": "FAIL"}')
+        return jsonify({"response": "FAIL"})
 
 ####################
 # Lobby
